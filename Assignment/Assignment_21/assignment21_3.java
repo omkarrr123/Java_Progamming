e9f22011b9d90bd62ca935edddbc26f624c03077
omkarrr123 <omkarvedpathak727@gmail.com>
2025-11-03 23:28:08 +0530

    Add files via upload
    ADD ASSIGNMENT_21 SOLUTION Q1-5

---
+++ Assignment/Assignment_21/assignment21_3.java
@@ -0,0 +1,21 @@
+class Logic
+{
+    void displayFactors(int num)
+    {
+        System.out.println("Factors of " + num + " are:");
+        for(int i = 1; i <= num; i++)
+        {
+            if(num % i == 0)
+                System.out.println(i);
+        }
+    }
+}
+
+class Program
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.displayFactors(12);
+    }
+}
